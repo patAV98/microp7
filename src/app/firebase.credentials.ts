6b0c235e6c5a2b9701171216caccef7e2efe1a68
patAV98 <kh.ff.love98@gmail.com>
2019-03-29 10:01:25 +0100

    Microp7

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBQ8iI2Vq0chmeMzDarzoQnN_GfSdEyYuY",
+    authDomain: "agendafirebase-288aa.firebaseapp.com",
+    databaseURL: "https://agendafirebase-288aa.firebaseio.com",
+    projectId: "agendafirebase-288aa",
+    storageBucket: "",
+    messagingSenderId: "918099356760"
+  };
---
+++ src/pages/nuevo-contacto/nuevo-contacto.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ContactService } from '../../services/contact.service'
+
+/**
+ * Generated class for the NuevoContactoPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-nuevo-contacto',
+  templateUrl: 'nuevo-contacto.html',
+})
+export class NuevoContactoPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private contactService: ContactService) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad NuevoContactoPage');
+  }
+
+  onAddContact(value: {nombre:string,organizacion:string,movil:string,correo:string}){
+
+    //this.contactService.addContact(value);
+
+    this.contactService.addContact(value).then(ref => {
+      console.log(ref.key);
+    });
+    this.navCtrl.pop();
+  }
+  
+}
